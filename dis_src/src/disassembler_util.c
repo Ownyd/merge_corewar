32c62e7a4cc16ecf98fc544d588bc468bdbfce2b
Lancelot OWCZARCZAK <lowczarc@student.42.fr>
2018-03-15 17:56:19 +0100

    norminette passé

--- dis_src/src/disassembler_util.c
+++ dis_src/src/disassembler_util.c
@@ -6,13 +6,13 @@
 /*   By: emarin <emarin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/08 18:40:03 by emarin            #+#    #+#             */
-/*   Updated: 2018/03/15 16:23:52 by emarin           ###   ########.fr       */
+/*   Updated: 2018/03/15 17:52:22 by lowczarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "disassembler.h"
 
-int		usage_disassembler()
+int		usage_disassembler(void)
 {
 	ft_putstr("usage:\n./disassembler champion.cor\n");
 	return (1);
